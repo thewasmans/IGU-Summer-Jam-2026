93eecb92469722751d28902be9af1758288d331b
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2026-07-14 19:54:34 +0200

    feat: add interraction system
    # Conflicts:
    #	scenes/main.tscn

--- scripts/managers/interract_area_manager.gd
+++ scripts/managers/interract_area_manager.gd
@@ -3,6 +3,9 @@ class_name InterractAreaManager
 
 @export var level: Node
 
+var game_ui: GameUI
+var current_area: Interract_Area
+
 func initialize(game_manager:GameManager):
 	super.initialize(game_manager)
 	var areas := level.get_tree().get_nodes_in_group("AreaInterract")
@@ -10,10 +13,26 @@ func initialize(game_manager:GameManager):
 		area.player_entered.connect(_on_player_entered.bind(area))
 		area.player_exited.connect(_on_player_exited)
 		area.player_completed.connect(_on_player_exited)
+	game_ui = _game_manager.game_ui_manager._game_ui
 		
 func _on_player_entered(area: Interract_Area):
-	if not area.completed:
-		_game_manager.game_ui_manager._game_ui.set_visible_interract_label(true)
+	current_area = area
+	if area.interract_type == "Pressed":
+		if not area.completed:
+			game_ui.set_visible_interract_label(true)
+	elif area.interract_type == "Stay":
+		if not area.completed:
+			game_ui.set_visible_interract_label(true)
+			game_ui.set_value_progress_interract(0)
+			game_ui.set_visible_progress_interract(true)
 
 func _on_player_exited():
-	_game_manager.game_ui_manager._game_ui.set_visible_interract_label(false)
+	game_ui.set_visible_interract_label(false)
+	game_ui.set_visible_progress_interract(false)
+
+func _process(delta: float):
+	if current_area:
+		if current_area.interract_started:
+			game_ui.set_value_progress_interract(current_area.timer.wait_time - current_area.timer.time_left)
+		else:
+			game_ui.set_value_progress_interract(0)
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -2,12 +2,21 @@ class_name GameUI
 extends Control
 
 @export var label_interract: Label
+@export var progress_interract: ProgressBar
 
 var _game_manager:GameManager
 
 func initialize(game_manager:GameManager):
 	_game_manager = game_manager
 	label_interract.hide()
+	progress_interract.hide()
 
 func set_visible_interract_label(enable: bool):
 	label_interract.visible = enable
+
+func set_visible_progress_interract(enable: bool):
+	progress_interract.visible = enable
+	
+func set_value_progress_interract(progress_value: float):
+	progress_interract.value = progress_value / progress_interract.max_value
+	
