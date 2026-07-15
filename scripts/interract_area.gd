d6fd74e3c3ff4bc68cd40e4c627208855b89f0ca
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2026-07-15 18:14:44 +0200

    feat: add fact système to interract

--- scripts/interract_area.gd
+++ scripts/interract_area.gd
@@ -11,7 +11,9 @@ signal player_completed
 ## [b]Entered[/b]: the interraction is completed when the player entered in the area
 ## [b]Wait[/b]: the interraction is completed when the player entered in the area and waiting the the stay time
 @export_enum("Pressed", "Stay", "Entered", "Wait") var interract_type: String = "Pressed"
+## Used for Stay and Wait [b]interract_type[/b]
 @export_custom(PROPERTY_HINT_NONE, "suffix:seconds") var stay_time: float = 5
+@export var locked_by_facts: Array[String]
 ## This field only use like a comment to help the level designer
 @export var description: String
 var timer : Timer:
@@ -21,8 +23,11 @@ var can_interract: bool
 var interract_started: bool
 var completed: bool
 
+func _ready():
+	Facts.set_fact(str(get_path()) + "_completed", "false")
+
 func _unhandled_input(event: InputEvent) -> void:
-	if can_interract and not completed:
+	if can_interract and not completed and not area_locked_by_fact():
 		if interract_type == "Pressed":
 			if event.is_action_pressed("action_interract"):
 				_complete_interract()
@@ -45,6 +50,7 @@ func _on_body_entered(_body):
 
 func _on_body_exited(_body):
 	can_interract = false
+	interract_started = false
 	%Timer.stop()
 	player_exited.emit()
 
@@ -52,5 +58,12 @@ func _on_timer_timeout():
 	_complete_interract()
 	
 func _complete_interract():
+	Facts.set_fact(str(get_path()) + "_completed", "true")
 	completed = true
 	player_completed.emit()
+
+func area_locked_by_fact() -> bool:
+	for fact in locked_by_facts:
+		if Facts.get_fact(fact) == false:
+			return true
+	return false
