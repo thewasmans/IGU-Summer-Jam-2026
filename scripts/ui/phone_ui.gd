23805adac9a2ec9563f9d86735172ed4bd989daa
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2026-07-15 20:07:02 +0200

    feat: add messages sms interractions

--- scripts/ui/phone_ui.gd
+++ scripts/ui/phone_ui.gd
@@ -4,16 +4,23 @@ extends Control
 @export var app_name_label: Label
 @export var home_app: PhoneApp
 @export var messages_app: PhoneMessagesApp
+@export var secours_app: PhoneSecoursApp
+@export var geoloc_app: PhoneGeolocApp
+@export var geoloc_icon: Control
 
 var apps: Array[PhoneApp]:
 	get:
-		return [home_app, messages_app]
+		return [home_app, messages_app, secours_app, geoloc_app]
 
 var _current_app_index: int = 0
 
 func _ready() -> void:
 	hide()
 	_update_current_app()
+	Facts.listen_fact(geoloc_app.fact_key, func(value: Variant, _previous: Variant) -> void:
+		geoloc_icon.visible = value == true
+	)
+	geoloc_icon.visible = Facts.get_fact(geoloc_app.fact_key) == true
 
 func open() -> void:
 	_current_app_index = 0
